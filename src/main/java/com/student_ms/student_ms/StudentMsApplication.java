b6848c91aa3b8457671478a6d26261fc64b41d40
Tedros Alem <tedsaasfaha@gmail.com>
2024-11-19 19:04:54 +0300

    first commit - student Ms initialization

---
+++ src/main/java/com/student_ms/student_ms/StudentMsApplication.java
@@ -0,0 +1,15 @@
+package com.student_ms.student_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMsApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!!!");
+		SpringApplication.run(StudentMsApplication.class, args);
+	}
+
+}
