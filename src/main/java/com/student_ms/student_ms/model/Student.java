5c0e225ec36dfc1162c1ce4a738c27c52cd676ee
Tedros Alem <tedsaasfaha@gmail.com>
2024-11-20 17:16:36 +0300

    [db connection resolved - MySql Driver dependency added]

--- src/main/java/com/student_ms/student_ms/model/Student.java
+++ src/main/java/com/student_ms/student_ms/model/Student.java
@@ -3,8 +3,10 @@ package com.student_ms.student_ms.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Student {
 
     @Id
@@ -14,4 +16,5 @@ public class Student {
     private String name;
 
     private String gender;
+
 }
