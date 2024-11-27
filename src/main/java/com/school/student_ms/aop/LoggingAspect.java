59dafb2f705b6fc063fa2af9a0710b2f726c0c9e
Tedros Alem <tedsaasfaha@gmail.com>
2024-11-27 19:19:49 +0300

    implement aop with LoggingAspect class responsible for logging time of execution for all classes & methods under service package.

---
+++ src/main/java/com/school/student_ms/aop/LoggingAspect.java
@@ -0,0 +1,33 @@
+package com.school.student_ms.aop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Around("execution(* com.school.student_ms.service..*(..))")
+    public Object printServiceLogger(ProceedingJoinPoint pjp) throws Throwable {
+        try {
+            long startingTime = System.currentTimeMillis();
+            Object obj = pjp.proceed();
+            long endingTime = System.currentTimeMillis();
+            log.info("From AOP: {} : {} milliseconds",
+                    pjp.getSignature(), (endingTime - startingTime));
+            return obj;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
